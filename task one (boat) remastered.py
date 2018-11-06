dfec1f3e0ddaba87b7bdd94be6c0ae3a9f80f60e
flare74 <42711924+flare74@users.noreply.github.com>
2018-11-06 11:03:38 +0400

    Update task one (boat) remastered.py

--- task one (boat) remastered.py
+++ task one (boat) remastered.py
@@ -13,62 +13,84 @@ onehour = 20
 halfhour = 12
 addhalfhour = False
 selection = True
+processing = True
+main = True
+check = True
 
-while selection:
-    while True:
-        try:
-            boatchoice = int(input('what boat do you want to chose?'))
-            timerented = float(input('what time do you want to take out the boat?'))
-            timeback = float(input('what time do you want to return the boat?'))
-            if timerented <= 17 and timerented >= 10 and timeback > 10 and timeback < 17 and boatchoice in range(1,11):
-                #print("asdf")
-                selection = False
-                break
+while main:
+    while selection:
+        while True:
+            try:
+                boatchoice = int(input('what boat do you want to chose?'))
+                timerented = float(input('what time do you want to take out the boat?'))
+                timeback = float(input('what time do you want to return the boat?'))
+                if timerented <= 17 and timerented >= 10 and timeback > 10 and timeback < 17 and boatchoice in range(1,11):
+                    #print("asdf")
+                    
+                    break
+                else:
+                    print('thats no valid time or boat')
+            except:
+                print('thats a fancy number you got')
+           
+    #check if half hour is needed by checking indexes of time in & out
+
+        timerequired = times.index(timeback) - times.index(timerented)
+        if timerequired%2 !=0:
+            addhalfhour = True
+        processing = True
+        check = True
+
+
+    #append if the boat is availble
+        while processing:
+            if boatchoice == 1:
+                for i in range(timerequired):
+                    if boat1[times.index(timerented)+ i] != 0:
+                        check = False
+                if check == False:
+                        print('the boat is unavailble at this time')
+                        selection = True
+                        processing = False
+                if addhalfhour == True:
+                    print('halfhour is true')
+                    for i in range(timerequired):
+                        boat1[times.index(timerented) + i] = onehour/2
+                        if i == timerequired - 1:
+                            boat1[(times.index(timerented) + i) + 1] = halfhour
+                            print(boat1)
+                            addhalfhour == False
+                    else:
+                        print('inside adding full hours')
+                        for i in range(timerequired):
+                            boat1[times.index(timerented) + i] = onehour/2
+                            boat1[times.index(timerented) + timerequired] = onehour/2
+                            print(boat1)
+
+        if boatchoice == 2:
+            for i in range(timerequired):
+                if boat1[times.index(timerented)+ i] != 0:
+                    check = False
+                if check == False:
+                    print('the boat is unavailble at this time')
+                    selection = True
+                    processing = False
+            if addhalfhour == True:
+                print('halfhour is true')
+                for i in range(timerequired):
+                    boat2[times.index(timerented) + i] = onehour/2
+                    if i == timerequired - 1:
+                        boat2[(times.index(timerented) + i) + 1] = halfhour
+                        print(boat2)
+                        addhalfhour == False
             else:
-                print('thats no valid time or boat')
-        except:
-            print('thats a fancy number you got')
-       
+                print('inside adding full hours')
+                for i in range(timerequired):
+                    boat2[times.index(timerented) + i] = onehour/2
+                boat2[times.index(timerented) + timerequired] = onehour/2
+                print(boat2)
 
 
-#check if half hour is needed by checking indexes of time in & out
-
-timerequired = times.index(timeback) - times.index(timerented)
-if timerequired%2 !=0:
-    addhalfhour = True
-
-
-#append if the boat is availble
-
-if boatchoice == 1:
-    for i in range(timerequired):
-        if boat1[times.index(timerented)+ i] != 0:
-            print('the boat is unavailble at this time')
-            selection =  True
-    if addhalfhour == True:
-        print('halfhour is true')
-        for i in range(timerequired):
-            boat1[times.index(timerented) + i] = onehour/2
-            if i == timerequired - 1:
-                boat1[(times.index(timerented) + i) + 1] = halfhour
-                print(boat1)
-                addhalfhour == False
-    else:
-        print('inside adding full hours')
-        for i in range(timerequired):
-            boat1[times.index(timerented) + i] = onehour/2
-        boat1[times.index(timerented) + timerequired] = onehour/2
-        print(boat1)
-
-if boatchoice == 2:
-    for i in range(timerequired):
-        if boat2[times.index(timerented)+ i] != 0:
-            print('the boat is unavailble at this time')
-    selection =  True
-    if addhalfhour == True:
-        print('halfhour is true')
-        for i in range(timerequired):
-            boat2[times.index(timerented) + i] = onehour/2
             if i == timerequired - 1:
                 boat2[(times.index(timerented) + i) + 1] = halfhour
                 print(boat2)
